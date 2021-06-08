b684adfccd7a7a1da3686688e4a9141af4835a59
ruucm <ruucm.a@gmail.com>
2021-06-09 03:39:04 +0900

    update loading styles

--- components/loading.tsx
+++ components/loading.tsx
@@ -14,36 +14,23 @@ const Loading: React.FC<{ isRouteChanging?: boolean }> = ({
         .container {
           opacity: ${isFinished ? 0 : 1};
           pointerevents: none;
-          transition: opacity ${animationDuration}ms linear;
+          transition: opacity 0ms linear;
         }
 
         .bar {
-          background: #29d;
-          height: 4px;
+          background: blue;
+          height: 20px;
           left: 0;
           margin-left: ${(-1 + progress) * 100}%;
           position: fixed;
-          top: 0;
+          bottom: 0;
           transition: margin-left ${animationDuration}ms linear;
           width: 100%;
           z-index: 1031;
         }
-
-        .spinner {
-          box-shadow: 0 0 10px #29d, 0 0 5px #29d;
-          display: block;
-          height: 100%;
-          opacity: 1;
-          position: absolute;
-          right: 0;
-          transform: rotate(3deg) translate(0px, -4px);
-          width: 100px;
-        }
       `}</style>
       <div className="container">
-        <div className="bar">
-          <div className="spinner" />
-        </div>
+        <div className="bar" />
       </div>
     </>
   )
