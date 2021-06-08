08dd7c0bdca23a527d454ec795579f3f0a7c8ea3
ruucm <ruucm.a@gmail.com>
2021-06-09 08:23:09 +0900

    seperate

--- pages/portfolio/_components/portfolio-section-card.tsx
+++ pages/portfolio/_components/portfolio-section-card.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { motion } from "framer-motion"
 import { styled } from "styletron-react"
 import { breakPoint } from "../../../consts"
-import { LazyLoadImage } from "./lazy-load-image"
+import { SectionCardImage } from "./section-card-image"
 
 const Grid = styled(motion.div, ({ $zoomed }: any) => {
   return {
@@ -45,7 +45,7 @@ export function PortfolioSectionCard({
 
   return (
     <Grid $zoomed={zoomed}>
-      <Image
+      <SectionCardImage
         img={img}
         grayscale={grayscale}
         zoomed={zoomed}
@@ -68,74 +68,6 @@ export function PortfolioSectionCard({
   )
 }
 
-const Wrap = styled("div", ({ $zoomed, $filter }: any) => {
-  return {
-    position: "relative",
-    width: "100%",
-    height: "0",
-    paddingTop: "66.66%",
-    filter: $filter,
-    ...getCursurStyles({ $zoomed }),
-  }
-})
-
-const GifImage = styled(motion.img, {
-  position: "absolute",
-  top: "0",
-  left: "0",
-  right: "0",
-  bottom: "0",
-  width: "100%",
-  height: "100%",
-  border: "1px solid",
-})
-
-const Image = ({ img, grayscale, zoomed, setZoomed }) => {
-  let filter = ""
-  if (grayscale) filter += "grayscale(1) brightness(0.5)"
-  const isGif = img.includes("gif")
-
-  return (
-    <Wrap $zoomed={zoomed} $filter={filter}>
-      <motion.div
-        animate={{ opacity: zoomed ? 1 : 0 }}
-        className="shade"
-        onClick={() => setZoomed(false)}
-      />
-      {isGif && (
-        <GifImage
-          src={img}
-          alt=""
-          onClick={() => setZoomed(!zoomed)}
-          layout
-          transition={defaultTransition}
-        />
-      )}
-      {!isGif && (
-        <LazyLoadImage
-          $as={motion.img}
-          imgUrl={`${img}.webp`}
-          placeholderUrl={`${img}-sharp.webp`}
-          fallbackUrl={`${img}-sharp.webp`}
-          onClick={() => setZoomed(!zoomed)}
-          layout
-          transition={defaultTransition}
-          style={{
-            position: "absolute",
-            top: "0",
-            left: "0",
-            right: "0",
-            bottom: "0",
-            width: "100%",
-            height: "100%",
-            border: "1px solid",
-          }}
-        />
-      )}
-    </Wrap>
-  )
-}
-
 function getZoomStyles({ $zoomed }) {
   if ($zoomed) {
     return {
@@ -144,18 +76,6 @@ function getZoomStyles({ $zoomed }) {
   } else {
     return {
       gridTemplateColumns: "1fr 1fr",
-    }
-  }
-}
-
-function getCursurStyles({ $zoomed }) {
-  if ($zoomed) {
-    return {
-      cursor: "zoom-out",
-    }
-  } else {
-    return {
-      cursor: "zoom-in",
     }
   }
 }
---
+++ pages/portfolio/_components/section-card-image.tsx
@@ -0,0 +1,91 @@
+import React from "react"
+import { useState } from "react"
+import { motion } from "framer-motion"
+import { styled } from "styletron-react"
+import { breakPoint } from "../../../consts"
+import { LazyLoadImage } from "./lazy-load-image"
+
+const Wrap = styled("div", ({ $zoomed, $filter }: any) => {
+  return {
+    position: "relative",
+    width: "100%",
+    height: "0",
+    paddingTop: "66.66%",
+    filter: $filter,
+    ...getCursurStyles({ $zoomed }),
+  }
+})
+
+const GifImage = styled(motion.img, {
+  position: "absolute",
+  top: "0",
+  left: "0",
+  right: "0",
+  bottom: "0",
+  width: "100%",
+  height: "100%",
+  border: "1px solid",
+})
+
+const defaultTransition = {
+  duration: 0.36,
+}
+
+export function SectionCardImage({ img, grayscale, zoomed, setZoomed }) {
+  let filter = ""
+  if (grayscale) filter += "grayscale(1) brightness(0.5)"
+  const isGif = img.includes("gif")
+
+  return (
+    <Wrap $zoomed={zoomed} $filter={filter}>
+      <motion.div
+        animate={{ opacity: zoomed ? 1 : 0 }}
+        className="shade"
+        onClick={() => setZoomed(false)}
+      />
+      {isGif && (
+        <GifImage
+          src={img}
+          alt=""
+          onClick={() => setZoomed(!zoomed)}
+          layout
+          transition={defaultTransition}
+        />
+      )}
+      {!isGif && (
+        <LazyLoadImage
+          $as={motion.img}
+          imgUrl={`${img}.webp`}
+          placeholderUrl={`${img}-sharp.webp`}
+          fallbackUrl={`${img}-sharp.webp`}
+          onClick={() => setZoomed(!zoomed)}
+          layout
+          transition={defaultTransition}
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            right: "0",
+            bottom: "0",
+            width: "100%",
+            height: "100%",
+            border: "1px solid",
+          }}
+        />
+      )}
+    </Wrap>
+  )
+}
+
+SectionCardImage.defaultProps = {}
+function getCursurStyles({ $zoomed }) {
+  if ($zoomed) {
+    return {
+      cursor: "zoom-out",
+    }
+  } else {
+    return {
+      cursor: "zoom-in",
+    }
+  }
+}
