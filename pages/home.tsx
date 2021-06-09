9da3e17b7ae36decf2896a9c69cf85517e00b03c
ruucm <ruucm.a@gmail.com>
2021-06-10 01:57:05 +0900

    fix texts

--- pages/home.tsx
+++ pages/home.tsx
@@ -6,42 +6,7 @@ import { AnimateSharedLayout, AnimatePresence } from "framer-motion"
 import { removeA } from "../utils/removeA"
 import { styled } from "styletron-react"
 
-const Grid = styled("div", {
-  display: "grid",
-  gridTemplateColumns: "1fr",
-  gap: "20px",
-})
-
-const Tag = styled("em", ({ $active }: any) => {
-  return {
-    position: "relative",
-    textDecoration: "none",
-    cursor: "pointer",
-    marginLeft: "10px",
-    "::before": {
-      content: "''",
-      position: "absolute",
-      width: "100%",
-      height: "1px",
-      bottom: 0,
-      left: 0,
-      backgroundColor: "blue",
-      visibility: $active ? "visible" : "hidden",
-      transform: $active ? "scaleX(1)" : "scaleX(0)",
-      transition: "all 0.3s ease-in-out 0s",
-    },
-  }
-})
-
-const Tags = styled("div", {
-  textAlign: "right",
-  marginBottom: "7px",
-})
-
 export function Page() {
-  const [data, setData] = useState(cardDatas)
-  const [tags, setTags] = useState(["develop", "design", "marketing"])
-
   return (
     <Layout home>
       <h3>Design Engineer</h3>
@@ -66,8 +31,7 @@ export function Page() {
           }}
           href="/projects"
         >
-          Design teams who want to close gap between their designs and real
-          products.
+          Design Teams
         </a>
         <div
           style={{
@@ -81,7 +45,7 @@ export function Page() {
             textAlign: "center",
           }}
         >
-          Designers who want to learn codes.
+          Techy Designers
         </div>
         <div
           style={{
@@ -95,7 +59,7 @@ export function Page() {
             textAlign: "center",
           }}
         >
-          UI designers
+          Prototypers
         </div>
         <div
           style={{
@@ -109,16 +73,9 @@ export function Page() {
             textAlign: "center",
           }}
         >
-          Content writers, who wants to integrate his writing experiences.
+          Writers
         </div>
       </div>
     </Layout>
   )
-}
-
-function tagFilter(tags, item) {
-  for (let i = 0; i < item.tags.length; i++) {
-    const tag = item.tags[i]
-    if (tags.includes(tag)) return true
-  }
 }
