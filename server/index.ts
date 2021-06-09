5b25b109b13b2fde12d65df0176311a058c7e27d
ruucm <ruucm.a@gmail.com>
2021-06-10 06:08:16 +0900

    change default port to 4000

--- server/index.ts
+++ server/index.ts
@@ -32,7 +32,7 @@ async function startServer() {
     res.status(result.statusCode).send(result.renderResult)
   })
 
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 4000
   app.listen(port)
   console.log(`Server running at http://localhost:${port}`)
 }
