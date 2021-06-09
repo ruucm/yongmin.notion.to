b7d42de604dfa761b3cdc9cfaf7a2074735534f1
ruucm <ruucm.a@gmail.com>
2021-06-09 16:49:15 +0900

    add FixedSize to LazyLoadImage

--- pages/portfolio/_components/lazy-load-image.tsx
+++ pages/portfolio/_components/lazy-load-image.tsx
@@ -1,6 +1,16 @@
 import React from "react"
 import useLazyImg from "../../../hooks/use-lazy-image"
 import dummy from "../../assets/dummy.png"
+import { styled } from "styletron-react"
+
+const FixedSize = styled("div", ({ $zoomed, $filter }: any) => {
+  return {
+    position: "relative",
+    width: "100%",
+    height: "0px",
+    paddingTop: "66.66%",
+  }
+})
 
 export function LazyLoadImage({
   imgUrl,
@@ -12,45 +22,43 @@ export function LazyLoadImage({
   layout,
   transition,
 }: any) {
-  const Comp: any = as
+  const ImageComp = styled(as, {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    width: "100%",
+    height: "100%",
+  })
 
   const { loading, imgSrc, onError } = useLazyImg(
     imgUrl,
-    dummy,
+    placeholderUrl,
     null,
     {},
     fallbackUrl
   )
-  // if (loading)
-  //   return (
-  //     <Comp
-  //       src={dummy}
-  //       alt="portfolio-image"
-  //       className={className}
-  //       onClick={onClick}
-  //       layout={layout}
-  //       transition={transition}
-  //     />
-  //   )
-  // else
-  //   return (
-  //     <Comp
-  //       src={imgSrc}
-  //       alt="portfolio-image"
-  //       className={className}
-  //       onClick={onClick}
-  //       layout={layout}
-  //       transition={transition}
-  //     />
-  //   )
+
   return (
-    <Comp
-      src={dummy}
-      alt="portfolio-image"
-      className={className}
-      onClick={onClick}
-      layout={layout}
-      transition={transition}
-    />
+    <FixedSize>
+      <ImageComp
+        src={dummy}
+        alt="portfolio-image"
+        className={className}
+        onClick={onClick}
+        layout={layout}
+        transition={transition}
+        style={{}}
+      />
+      <ImageComp
+        src={imgSrc}
+        alt="portfolio-image"
+        className={className}
+        onClick={onClick}
+        layout={layout}
+        transition={transition}
+      />
+    </FixedSize>
   )
 }
