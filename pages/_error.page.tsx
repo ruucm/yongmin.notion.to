b2950357e6a0996c5af5237a7a0c917f234b396f
ruucm <ruucm.a@gmail.com>
2021-06-10 04:05:20 +0900

    add layout to 404

--- pages/_error.page.tsx
+++ pages/_error.page.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React from "react"
+import { Layout } from "../components"
 
-export { Page };
+export { Page }
 
 function Page({ is404 }: { is404: boolean }) {
   if (is404) {
     return (
-      <>
+      <Layout>
         <h1>404 Page Not Found</h1>
         This page could not be found.
-      </>
-    );
+      </Layout>
+    )
   } else {
     return (
-      <>
+      <Layout>
         <h1>500 Internal Server Error</h1>
         Something went wrong.
-      </>
-    );
+      </Layout>
+    )
   }
 }
