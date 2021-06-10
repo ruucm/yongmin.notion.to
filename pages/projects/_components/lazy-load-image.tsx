206c8cee56461f4279b76826f3b97671c2499950
ruucm <ruucm.a@gmail.com>
2021-06-10 23:50:19 +0900

    refactor: replace to AspectRatio

--- pages/projects/_components/lazy-load-image.tsx
+++ pages/projects/_components/lazy-load-image.tsx
@@ -2,16 +2,16 @@ import { motion } from "framer-motion"
 import React from "react"
 import { styled } from "styletron-react"
 import { imageBasePath } from "../../../consts"
-import { Responsive } from "../../../utils/styled-components"
+import { AspectRatio } from "../../../utils/aspect-ratio"
 
 const Image = styled(motion.img, {
+  width: "100%",
+  height: "100%",
   position: "absolute",
   top: "0",
   left: "0",
   right: "0",
   bottom: "0",
-  width: "100%",
-  height: "100%",
 })
 
 export function LazyLoadImage({
@@ -23,36 +23,44 @@ export function LazyLoadImage({
   transition,
 }: any) {
   return (
-    <Responsive>
-      <Image
-        src={placeholderImage}
-        alt="projects-image-placholder"
-        className={className}
-        onClick={onClick}
-        layout={layout}
-        transition={transition}
-      />
-      <picture>
-        <source
-          srcSet={`${imageBasePath}/${imageName}.webp`}
-          type="image/webp"
-        />
-        <source srcSet={`${imageBasePath}/${imageName}.png`} type="image/png" />
-        <source srcSet={`${imageBasePath}/${imageName}.jpg`} type="image/jpg" />
-        <source
-          srcSet={`${imageBasePath}/${imageName}.jpeg`}
-          type="image/jpeg"
-        />
-
+    <AspectRatio ratio={1720 / 1053}>
+      <div>
         <Image
-          src={`${imageBasePath}/${imageName}.png`}
-          alt="projects-image"
+          src={placeholderImage}
+          alt="projects-image-placholder"
           className={className}
           onClick={onClick}
           layout={layout}
           transition={transition}
         />
-      </picture>
-    </Responsive>
+        <picture>
+          <source
+            srcSet={`${imageBasePath}/${imageName}.webp`}
+            type="image/webp"
+          />
+          <source
+            srcSet={`${imageBasePath}/${imageName}.png`}
+            type="image/png"
+          />
+          <source
+            srcSet={`${imageBasePath}/${imageName}.jpg`}
+            type="image/jpg"
+          />
+          <source
+            srcSet={`${imageBasePath}/${imageName}.jpeg`}
+            type="image/jpeg"
+          />
+
+          <Image
+            src={`${imageBasePath}/${imageName}.png`}
+            alt="projects-image"
+            className={className}
+            onClick={onClick}
+            layout={layout}
+            transition={transition}
+          />
+        </picture>
+      </div>
+    </AspectRatio>
   )
 }
