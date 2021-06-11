d082232ade530ccb693aa7a2feab980186831034
ruucm <ruucm.a@gmail.com>
2021-06-12 03:22:10 +0900

    add images to notion-cms

--- pages/projects/notion-cms/notion-cms.tsx
+++ pages/projects/notion-cms/notion-cms.tsx
@@ -4,6 +4,7 @@ import { AnimateSharedLayout } from "framer-motion"
 import { Summary } from "../_components/summary"
 import { SubTitle } from "../_components/sub-title"
 import { PostLayout } from "../../../components/post-layout"
+import { Gallery } from "../_components/gallery"
 
 export function Page({ slug }) {
   return (
@@ -11,6 +12,17 @@ export function Page({ slug }) {
       <Summary slug={slug} />
       <AnimateSharedLayout>
         <SubTitle>framer-packages</SubTitle>
+        <Gallery
+          images={[
+            "notion-seli-01",
+            "notion-seli-02",
+            "notion-seli-03",
+            "notion-cms-progress-1",
+            "notion-cms-progress-2",
+            "notion-seli-04",
+            "notion-seli-05",
+          ]}
+        />
       </AnimateSharedLayout>
     </PostLayout>
   )
