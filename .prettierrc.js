0f3862d664d4b2d55a2940bdb73d004d22333221
ruucm <ruucm.a@gmail.com>
2023-02-22 00:15:58 +0900

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  printWidth: 120,
+  // singleQuote: true,
+  // trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  useTabs: false,
+  arrowParens: "always"
+}
